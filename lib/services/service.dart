d11880cf09165d498ab781c7e0d63013af6c016d
unknown <aqshalzuhdi2837@gmail.com>
2024-05-25 21:19:44 +0700

    initial

--- lib/services/service.dart
+++ lib/services/service.dart
@@ -1,5 +1,7 @@
 import 'package:technotafet/entities/entity.dart';
 import 'package:technotafet/objectbox.g.dart';
+import 'package:udp/udp.dart';
 
 part 'athlete_service.dart';
 part 'exercise_service.dart';
+part 'udp_service.dart';
---
+++ lib/services/udp_service.dart
@@ -0,0 +1,17 @@
+part of 'service.dart';
+
+class UdpService {
+  // Future<Stream<Object>> readUDP() async {
+  //   var receiver = await UDP.bind(
+  //     Endpoint.loopback(
+  //       port: const Port(65002),
+  //     ),
+  //   );
+
+  //   receiver.asStream().listen((output) {
+  //     var str = String.fromCharCodes(output!.data);
+  //     // stdout.write(str);
+  //     print(str);
+  //   });
+  // }
+}
